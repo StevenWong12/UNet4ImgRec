d19ce119ff6d70bb0cdcb70fa860efb4bdf16226
adminmgr@protonmail.com <adminmgr123>
2023-02-23 09:53:30 +0800

    Remove n_classes arg

--- unet/unet_model.py
+++ unet/unet_model.py
@@ -4,10 +4,10 @@ from .unet_parts import *
 
 
 class UNet(nn.Module):
-    def __init__(self, n_channels, n_classes, bilinear=False):
+    def __init__(self, n_channels, bilinear=False):
         super(UNet, self).__init__()
         self.n_channels = n_channels
-        self.n_classes = n_classes
+        self.n_classes = 1
         self.bilinear = bilinear
 
         self.inc = (DoubleConv(n_channels, 64))
@@ -20,7 +20,7 @@ class UNet(nn.Module):
         self.up2 = (Up(512, 256 // factor, bilinear))
         self.up3 = (Up(256, 128 // factor, bilinear))
         self.up4 = (Up(128, 64, bilinear))
-        self.outc = (OutConv(64, n_classes))
+        self.outc = (OutConv(64, self.n_classes))
         # 64 -> RGB
         self.out_img = (OutConv(64, 3))
 
